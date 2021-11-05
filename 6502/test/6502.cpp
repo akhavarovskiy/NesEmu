cf332387f95006b111c84587394f52cf69b3ea16
Alex Khavarovskiy <a.khavarovskiy@gmail.com>
2021-11-05 12:40:02 -0700

    Addded ADC Immediate Test

--- 6502/test/6502.cpp
+++ 6502/test/6502.cpp
@@ -3,10 +3,64 @@
 #include <iostream>
 #include <6502.hpp>
 #include <6502def.hpp>
-
 #include <cstring>
 
 using namespace MOS6502;
+
+TEST(ADC, IMMEDIATE)
+{
+    TestCPU t;
+    t.SetBootAddress(0x100);
+    t.NI_Write(0x100, MOS6502_ADC_IMMEDIATE); /* Set the instruction at boot address */
+    t.Reset();                                  /* Reset the CPU */
+    /* Verify boot address*/
+    MOS6502::REG & r = t.Register();            /* Get the registers */
+    EXPECT_EQ(r.PC, 0x100);                     /* Verify that the program counter is at 0x100 */
+    /*
+    ============================================
+        Sub Test 1
+    ============================================*/
+    t.SetTicks(0);
+    r.A    = 0xFF;              /* Set the accumulator */
+    r.PS.C = 0x01;
+    t.NI_Write(0x101, 0xFF);    /* Set the operand */
+    t.Step();                   /* Run Single instruction */
+    EXPECT_EQ(r.A, 0xFF);       /* Verify Accumulator */
+    EXPECT_EQ(r.PS.Z, 0);       /* Verify Flags */
+    EXPECT_EQ(r.PS.N, 1);
+    EXPECT_EQ(r.PS.C, 1);
+    EXPECT_EQ(t.GetTicks(), 2); /* Verify Cycle Count */
+    /*
+    ============================================
+        Sub Test 2
+    ============================================*/
+    t.Reset();
+    t.SetTicks(0);
+    r.A    = 0x00;              /* Set the accumulator */
+    r.PS.C = 0x00;
+    t.NI_Write(0x101, 0x00);    /* Set the operand */
+    t.Step();                   /* Run Single instruction */
+    EXPECT_EQ(r.A, 0x00);       /* Verify Accumulator */
+    EXPECT_EQ(r.PS.Z, 1);       /* Verify Flags */
+    EXPECT_EQ(r.PS.N, 0);
+    EXPECT_EQ(r.PS.C, 0);
+    EXPECT_EQ(t.GetTicks(), 2); /* Verify Cycle Count */
+    /*
+    ============================================
+        Sub Test 3
+    ============================================*/
+    t.Reset();
+    t.SetTicks(0);
+    r.A    = 0x80;              /* Set the accumulator */
+    r.PS.C = 0x00;
+    t.NI_Write(0x101, 0x00);    /* Set the operand */
+    t.Step();                   /* Run Single instruction */
+    EXPECT_EQ(r.A, 0x80);       /* Verify Accumulator */
+    EXPECT_EQ(r.PS.Z, 0);       /* Verify Flags */
+    EXPECT_EQ(r.PS.N, 1);
+    EXPECT_EQ(r.PS.C, 0);
+    EXPECT_EQ(t.GetTicks(), 2); /* Verify Cycle Count */
+}
 
 TEST(ASL, ACCUMULATOR) {
     TestCPU t;
